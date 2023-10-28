931c56aee2042a2dcef660374f59d0d22a4f6f78
Zane Rockenbaugh <zane@liquid-labs.com>
2023-10-27 20:24:26 -0500

    lint fixes

--- src/handlers/credentials/test/list.test.js
+++ src/handlers/credentials/test/list.test.js
@@ -1,33 +1,28 @@
-/* global afterAll beforeAll describe expect jest test */
-import * as fsPath from 'node:path'
-
-import { CredentialsDB } from '@liquid-labs/liq-credentials-db'
-
+/* global describe expect test */
 import { func } from '../list'
 
 describe('GET:/credentials/list', () => {
-  const listData = [{ name: 'GITHUB_SSH' }, { name: 'GITHUB_API' }]
+  const listData = [{ name : 'GITHUB_SSH' }, { name : 'GITHUB_API' }]
   const credentialsDBMock = {
-    list: () => listData,
-    verifyCreds: () => {},
+    list        : () => listData,
+    verifyCreds : () => {}
   }
-  const appMock = { ext: { credentialsDB : credentialsDBMock }}
+  const appMock = { ext : { credentialsDB : credentialsDBMock } }
 
   const reqMock = {
-    vars: { verify: false },
-    accepts: () => 'application/json'
+    vars    : { verify : false },
+    accepts : () => 'application/json'
   }
 
   let result
   const resMock = {
-    json: (json) => { result = json },
+    json : (json) => { result = json }
   }
 
   test('lists the DB credential entries', () => {
-    const handler = func({ app: appMock })
+    const handler = func({ app : appMock })
     handler(reqMock, resMock)
 
     expect(result).toEqual(listData)
   })
-
 })
--- src/setup.mjs
+++ src/setup.mjs
@@ -5,7 +5,7 @@ import { CredentialsDB, CREDS_PATH_STEM } from '@liquid-labs/liq-credentials-db'
 
 const setup = async({ app, cache, model }) => {
   const credsDir = fsPath.join(app.ext.serverHome, CREDS_PATH_STEM)
-  await fs.mkdir(credsDir, { recursive: true })
+  await fs.mkdir(credsDir, { recursive : true })
 
   const credentialsDB = new CredentialsDB({ app, cache })
   app.ext.credentialsDB = credentialsDB
@@ -14,7 +14,7 @@ const setup = async({ app, cache, model }) => {
 }
 
 const setupPathResolvers = ({ app, model }) => {
-    app.ext.pathResolvers.credential = {
+  app.ext.pathResolvers.credential = {
     bitReString    : '(?:[A-Z0-9][A-Z0-9_]*)',
     optionsFetcher : () => app.ext.credentialsDB.listSupported().map(({ key }) => key)
   }
